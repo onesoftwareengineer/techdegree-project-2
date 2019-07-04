dbbb113beba8b657715c050424b3717fe1bce173
onesoftwareengineer <48726358+onesoftwareengineer@users.noreply.github.com>
2019-07-04 13:11:02 +0300

    search js removed
    all js is in one file

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function newElement(elementTag, elementClass) {
    return element;
 }
 
-//function that fetches all the people that are currently listed in the ul
+// function that fetches all the people that are currently listed in the ul
 function fetchCurrentPeople () {
    const currentPeople = [];
    for (let x=0; x < ul.children.length; x++) {
@@ -21,7 +21,7 @@ function fetchCurrentPeople () {
    return currentPeople;
 }
 
-//function showPage adds to first ul people that belong only to that page as sepparate li's 
+// function showPage adds to first ul people that belong only to that page as sepparate li's 
 //to speed things up, instead of hiding all the other elements, it just displays the one's that are needed
 function showPage (pageNumber, currentPeople) {
    ul.innerHTML = '';
@@ -30,7 +30,7 @@ function showPage (pageNumber, currentPeople) {
    }   
 }; 
 
-//appendPageLinks function that build the navigation div below the people names
+// function appendPageLinks function that build the navigation div below the people names
 function appendPageLinks () {
    const numberOfPages = Math.ceil(ul.children.length / 10);
    let currentPeople = fetchCurrentPeople();
@@ -55,8 +55,7 @@ function appendPageLinks () {
    addFunctionalityToPageLinks(navigationDiv, currentPeople);
 };
 
-
-// addFunctionalityToPageLinks function adds an event listener on the page number navigation 
+// function addFunctionalityToPageLinks function adds an event listener on the page number navigation 
 // it triggers the showPage function for the clicked page number and also changes css classes
 function addFunctionalityToPageLinks (navigationDiv, currentPeople) {
    navigationDiv.addEventListener('click', (event) => {
@@ -83,9 +82,45 @@ function addFunctionalityToPageLinks (navigationDiv, currentPeople) {
    });
 }
 
-//function appendPage is called after DOM load because script is included before body close in index html file
+//function searchFunction searches through the initial list people names and displays people found
+function searchFunction(searchedString) {
+    //clears page li elements from people ul list
+    ul.innerHTML = '';
+    //removes current pagination div
+    const pagination = document.querySelector('div.pagination');
+    ul.parentNode.removeChild(pagination);
+
+    for(let x=0; x < initialPeople.length; x++) {
+        let name = initialPeople[x].firstElementChild.firstElementChild.nextElementSibling.innerText;
+        if( name.indexOf(searchedString) != -1 ) {
+            ul.appendChild(initialPeople[x]);
+        }
+    }
+    appendPageLinks();
+    //if no people with similar names to the string are found, message is displayed
+    if(ul.innerHTML === '') {
+        ul.innerHTML = `Nope, there's nobody registered with that name. Try again.`;
+    }
+};
+
+//function createSearchBox creates the search box and adds functionality
+function createSearchBox () {
+      const header = document.querySelector('div.page-header');
+      const searchDiv = newElement('div','student-search');
+      searchDiv.innerHTML = `
+            <input placeholder="Search for students...">
+            <button>Search</button>
+      `;
+      header.appendChild(searchDiv);
+      
+      //appending functionality to the search button, so it triggers the search function and sends the string searched by the user as an argument 
+      searchDiv.addEventListener('input', (event) => {
+      const searchString = searchDiv.children[0].value;
+      searchFunction(searchString);
+   });
+}
+
+
+/* --- calling functions --- */
 appendPageLinks();
-
-
-
-
+createSearchBox();
