9e58a9dde1f31a860733766b8701832858c43f8a
onesoftwareengineer <48726358+onesoftwareengineer@users.noreply.github.com>
2019-07-04 12:29:49 +0300

    project final updates
    added serach.js which adds search functionality

--- js/script.js
+++ js/script.js
@@ -1,50 +1,91 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+/* --- global variables --- */
+const ul = document.querySelector('ul.student-list');
+//copies all the initial people li's into an array
+let initialPeople = fetchCurrentPeople();
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/* --- functions --- */
+//function used to create new element of given tagname and class
+function newElement(elementTag, elementClass) {
+   let element = document.createElement(elementTag);
+   element.className = elementClass;
+   return element;
+}
+
+//function that fetches all the people that are currently listed in the ul
+function fetchCurrentPeople () {
+   const currentPeople = [];
+   for (let x=0; x < ul.children.length; x++) {
+      currentPeople[x] = ul.children[x];
+   }
+   return currentPeople;
+}
+
+//function showPage adds to first ul people that belong only to that page as sepparate li's 
+//to speed things up, instead of hiding all the other elements, it just displays the one's that are needed
+function showPage (pageNumber, currentPeople) {
+   ul.innerHTML = '';
+   for(let x = (pageNumber-1)*10; x < pageNumber*10 && x < currentPeople.length; x++) {
+      ul.appendChild(currentPeople[x]);
+   }   
+}; 
+
+//appendPageLinks function that build the navigation div below the people names
+function appendPageLinks () {
+   const numberOfPages = Math.ceil(ul.children.length / 10);
+   let currentPeople = fetchCurrentPeople();
+   let navigationDiv = newElement('div', 'pagination');
+   let navigation = '<ul>';
+   for (let x=0; x<numberOfPages; x++)
+   {
+      //if x is not the first page, active class will note be applied 
+      if(x) {
+         navigation += `<li><a href="#">${x+1}</a></li>`;
+      }
+      //otherwise it means x is 0 so the first page, so active class needs to be applied
+      else {
+         navigation += `<li><a class="active" href="#">${x+1}</a></li>`;
+         //if it's the first page then showPage function will be called to display the first page
+         showPage(1, currentPeople);
+      }
+   }
+   navigation += '</ul>';
+   navigationDiv.innerHTML = navigation;
+   ul.parentNode.appendChild(navigationDiv);
+   addFunctionalityToPageLinks(navigationDiv, currentPeople);
+};
+
+
+// addFunctionalityToPageLinks function adds an event listener on the page number navigation 
+// it triggers the showPage function for the clicked page number and also changes css classes
+function addFunctionalityToPageLinks (navigationDiv, currentPeople) {
+   navigationDiv.addEventListener('click', (event) => {
+      //checks if the user clicked on a page number link from the page navigation 
+      if(event.target.tagName === 'A') {
+         const clickedPage = event.target;
+         const clickedPageNumber = parseInt(event.target.innerText);
+         const clickedPageNumberIndex = clickedPageNumber - 1;
+         const allPageNumbers = clickedPage.parentNode.parentNode.children;
+         
+         //triggers the show page function and sends page number to it
+         showPage(clickedPageNumber, currentPeople);
+         
+         //goes through all page numbers and applies active class only to the page number that was clicked
+         for(let x = 0; x < allPageNumbers.length; x++) {
+            if( x !== clickedPageNumberIndex ) {
+               allPageNumbers[x].firstElementChild.classList.remove('active');
+            }
+            else if( x === clickedPageNumberIndex ) {
+               allPageNumbers[x].firstElementChild.className = 'active';
+            }
+         }
+      }
+   });
+}
+
+//function appendPage is called after DOM load because script is included before body close in index html file
+appendPageLinks();
 
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
